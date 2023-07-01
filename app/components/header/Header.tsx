c4d78d6e8e858bf75d074b6ae62d23be35a43f5f
Michael Chan <mhcchan@ucdavis.edu>
2023-07-01 12:09:41 -0700

    feat: header, hero

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Box, Button, Flex } from '@chakra-ui/react';
+import Link from 'next/link';
+import { PiGlobe } from 'react-icons/pi';
+
+export default function Header() {
+  return (
+    <Flex h="52px" bg="white" justify="space-between" align="center" px="1em">
+      <Box h="0.8em">
+        <Link href="#">
+          <svg
+            height="100%"
+            style={{ inlineSize: '100%' }}
+            viewBox="0 0 342 35"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 .1a9.7 9.7 0 0 0 7 7h11l.5.1v27.6h6.8V7.3L26 7h11a9.8 9.8 0 0 0 7-7H0zm238.6 0h-6.8v34.8H263a9.7 9.7 0 0 0 6-6.8h-30.3V0zm-52.3 6.8c3.6-1 6.6-3.8 7.4-6.9l-38.1.1v20.6h31.1v7.2h-24.4a13.6 13.6 0 0 0-8.7 7h39.9v-21h-31.2v-7h24zm116.2 28h6.7v-14h24.6v14h6.7v-21h-38zM85.3 7h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 13.8h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 14.1h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zM308.5 7h26a9.6 9.6 0 0 0 7-7h-40a9.6 9.6 0 0 0 7 7z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </Link>
+      </Box>
+      <Button variant="ghost" leftIcon={<PiGlobe size={25} />} fontWeight={500}>
+        US
+      </Button>
+    </Flex>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import { Inter } from 'next/font/google';
 
+import Header from './components/header/Header';
 import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
