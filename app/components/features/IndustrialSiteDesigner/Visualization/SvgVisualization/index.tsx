35a9842b08c2bdc587003fafdbd13b408b95dc44
Michael Chan <mhcchan@ucdavis.edu>
2023-07-03 01:11:47 -0700

    chore: implements throttling for performance

--- app/components/features/IndustrialSiteDesigner/Visualization/SvgVisualization/index.tsx
+++ app/components/features/IndustrialSiteDesigner/Visualization/SvgVisualization/index.tsx
@@ -1,75 +1,21 @@
 import { SVG } from '@svgdotjs/svg.js';
 import * as d3 from 'd3';
-import { useEffect, useLayoutEffect, useRef } from 'react';
+import { throttle } from 'lodash';
+import { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
 
 import { useStore } from '@/app/lib/store';
+import { drawChart } from '@/app/utils/2dVisualization';
 import { greedyBalancing } from '@/app/utils/greedyBalancing';
 
 export function SvgVisualization() {
   const ref = useRef(null);
   const state = useStore();
 
-  const { maxLength, flattenedPlots } = greedyBalancing(state, 10, 100);
-
-  const drawChart = () => {
-    const width = 640;
-    const height = 400;
-    const marginTop = 20;
-    const marginRight = 20;
-    const marginBottom = 30;
-    const marginLeft = 40;
-    const data = [
-      { name: '1', data: [1, 2, 3, 4] },
-      { name: '2', data: [1, 2, 3, 5] },
-    ];
-    const colourScale = ['red', 'blue', 'yellow', 'pink', 'black'];
-
-    d3.select(ref.current).selectAll('*').remove();
-
-    const svg = d3
-      .select(ref.current)
-      .append('svg')
-      .attr('width', width + marginLeft + marginRight)
-      .attr('height', height + marginBottom + marginTop)
-      .append('g')
-      .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
-
-    const x = d3.scaleLinear().range([0, width]).domain([0, 100]);
-    const y = d3
-      .scaleLinear()
-      .range([height, 0])
-      .domain([Math.max(maxLength, 100), 0]);
-
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
-
-    svg
-      .selectAll('rect')
-      .data(flattenedPlots)
-      .enter()
-      .append('g')
-      .append('rect')
-      .attr('x', (d) => {
-        console.log(d.plot.x);
-        return d.plot.x * 64;
-      })
-      .attr('y', (d) => {
-        console.log('d.plot.y');
-        console.log(d.plot.y);
-        return d.plot.y;
-      })
-      .attr('width', 10 * (width / 100) - 8)
-      .attr('height', (d) => d.dimensions.length * 1)
-      .attr('fill', (d) => d.plot.color)
-      .style('border', '1px solid black');
-
-    svg.append('g').call(xAxis);
-    svg.append('g').call(yAxis);
-  };
+  const throttledDrawChart = useMemo(() => throttle(drawChart, 1_500), []);
 
   useLayoutEffect(() => {
-    drawChart();
-  }, [flattenedPlots]);
+    throttledDrawChart(state, ref);
+  }, [state, throttledDrawChart]);
 
   return <div id="svg_target" ref={ref} />;
 }
---
+++ app/utils/2dVisualization.ts
@@ -0,0 +1,63 @@
+import * as d3 from 'd3';
+import { MutableRefObject } from 'react';
+
+import { Store } from '../lib/store';
+import { greedyBalancing } from './greedyBalancing';
+
+/**
+ * Inefficient
+ * @param state
+ */
+export const drawChart = (state: Store, ref: MutableRefObject<null>) => {
+  const { maxLength, flattenedPlots } = greedyBalancing(state, 10, 100);
+
+  const width = 640;
+  const height = 400;
+  const marginTop = 20;
+  const marginRight = 20;
+  const marginBottom = 30;
+  const marginLeft = 40;
+
+  d3.select(ref.current).selectAll('*').remove();
+
+  const svg = d3
+    .select(ref.current)
+    .append('svg')
+    .attr('width', width + marginLeft + marginRight)
+    .attr('height', height + marginBottom + marginTop)
+    .append('g')
+    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+
+  const x = d3.scaleLinear().range([0, width]).domain([0, 100]);
+  const y = d3
+    .scaleLinear()
+    .range([height, 0])
+    .domain([Math.max(maxLength, 100), 0]);
+
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3.axisLeft(y);
+
+  svg
+    .selectAll('rect')
+    .data(flattenedPlots)
+    .enter()
+    .append('g')
+    .append('rect')
+    .attr('x', (d) => {
+      console.log(d.plot.x);
+      return d.plot.x * 64;
+    })
+    .attr('y', (d) => {
+      console.log('d.plot.y');
+      console.log(d.plot.y);
+      return d.plot.y;
+    })
+    .attr('width', 10 * (width / 100) - 8)
+    .attr('height', (d) => d.dimensions.length * 1)
+    // .transition()
+    .attr('fill', (d) => d.plot.color)
+    .style('border', '1px solid black');
+
+  svg.append('g').call(xAxis);
+  svg.append('g').call(yAxis);
+};
