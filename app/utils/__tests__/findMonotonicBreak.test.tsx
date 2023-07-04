04cb37f877e8b1913c8580084411a26134efdc7b
Michael Chan <mhcchan@ucdavis.edu>
2023-07-04 12:31:06 -0700

    test(util): adds statements for monotonic util

--- app/utils/__tests__/findMonotonicBreak.test.tsx
+++ app/utils/__tests__/findMonotonicBreak.test.tsx
@@ -3,6 +3,10 @@ import { findMonotonicBreak } from '../findMonotonicBreak';
 describe('findMonotonicBreak', () => {
   it('finds the monotonic break index', () => {
     expect(findMonotonicBreak([9, 8, 8, 7, 4, 6, 2, 1])).toEqual(5);
+
+    expect(findMonotonicBreak([9, 10, 8, 7, 4, 6, 2, 1])).toEqual(1);
+
+    expect(findMonotonicBreak([9, 8, 8, 7, 6, 6, 2, 10])).toEqual(7);
   });
 
   it('returns -1 if no monotonic break is found', () => {
